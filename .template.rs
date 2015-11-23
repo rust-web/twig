e1d2cadd68368f04cd7322d52c8d21f2abdd7e80
Colin Kiegel <kiegel@gmx.de>
2015-11-23 22:18:51 +0100

    template for new rust files

---
+++ .template.rs
@@ -0,0 +1,18 @@
+// This file is part of rust-web/twig
+//
+// For the copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+/// Twig library for rust
+///
+/// @author NAME <EMAIL>
+
+/////////////
+// imports //
+/////////////
+
+
+
+/////////////
+// exports //
+/////////////
